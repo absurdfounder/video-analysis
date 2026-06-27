f604ce4beaed1d08e4eeb878efa6531a78114f8d
usman724 <usmanpervaiz724@gmail.com>
2026-06-27 10:29:52 +0500

    Update Hetzner IP in wrangler.toml and fix default port in server.js

--- app/server.js
+++ app/server.js
@@ -11,7 +11,7 @@ const crypto = require('crypto');
 const { execFile } = require('child_process');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3007;
 
 app.use((req, res, next) => {
   const origin = process.env.CORS_ORIGIN || '*';
